c03654bb7561f6950ba34406c76a85283f367faa
IkkiArtz <65777252+IkkiArtz@users.noreply.github.com>
2021-02-13 12:04:15 -0300

    up

--- ProjetoDownload/main.py
+++ ProjetoDownload/main.py
@@ -2,17 +2,26 @@ import time
 
 def projectdownload():
     # Data extraction
-    print('What is the size file? (in MegaByte)')
-    sizeFile = int(input('-> '))
+    print('What is the size file? (in M = MegaByte, G = GigaByte)')
+    sizeFile = input('-> ')
+    sizeFile = sizeFile.lower()
     print('What is the your internet speed? (in MegaBite)')
-    netSpeed = int(input('-> '))
+    netSpeed = input('-> ')
 
     # MegaBite for MegaByte
-    biteByteSpeed = (netSpeed) / 8
+    biteByteSpeed = int(netSpeed) / 8
     if biteByteSpeed == 1.0:
         biteByteSpeed = 0.9
     else:
         print('Ocorreu um erro')
+
+    # Gigabyte to Megabyte
+    if 'g' in sizeFile:
+        sizeFile = sizeFile.replace(' ','')
+        sizeFile = sizeFile.replace('g','')
+        sizeFile = sizeFile * 1000
+        sizeFile = float(sizeFile)
+        print(sizeFile)
 
     # Calc time of download
     timeSec = float(sizeFile) / float(biteByteSpeed)
@@ -28,3 +37,5 @@ def projectdownload():
     print(f'The download time of {sizeFile} Megabyte is: \n {timeHour} horas; \n {timeMin} Minutos; \n {timeSec} Segundos.')
 
     time.sleep(5.0)
+
+projectdownload()
