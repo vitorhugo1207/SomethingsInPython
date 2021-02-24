f4040493bc230da1bde592587b2f41d2b2373821
IkkiArtz <65777252+IkkiArtz@users.noreply.github.com>
2021-02-24 11:39:28 -0300

    up

---
+++ Random.py
@@ -0,0 +1,8 @@
+import random
+
+thing1 = input('Digite alguma coisa: ')
+thing2 = input('Digite outra coisa: ')
+thing3 = input('Digite outra coisa: ')
+thing4 = input('Digite outra coisa: ')
+total = random.choice([thing1, thing2, thing3, thing4])
+print(total)
